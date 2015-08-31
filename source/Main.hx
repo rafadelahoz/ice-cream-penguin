54279b866fd9a7d1813e34abd9d374389d778655
Rafa de la Hoz <rafa.delahoz@gmail.com>
2015-08-31 17:07:33 +0200

    Drafted some kind of backdrop for the letterboxed game

--- source/Main.hx
+++ source/Main.hx
@@ -8,15 +8,21 @@ import flash.Lib;
 import flixel.FlxGame;
 import flixel.FlxState;
 
+import openfl.display.Bitmap;
+import openfl.Assets;
+
 class Main extends Sprite 
 {
-	var gameWidth:Int = 240; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameWidth:Int = 240; //192; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 160; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = MenuState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var zoom:Float = 1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 50; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	
+	var bitmap : Bitmap;
+	var game : FlxGame;
 	
 	// You can pretty much ignore everything from here on - your code should go in your states.
 	
@@ -37,6 +43,23 @@ class Main extends Sprite
 		{
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
+		
+		var bitmapData = Assets.getBitmapData ("assets/images/bg.jpg");
+        bitmap = new Bitmap (bitmapData);
+        addChild (bitmap);
+
+        bitmap.width = Lib.current.stage.stageWidth;
+		bitmap.height = Lib.current.stage.stageHeight;
+		
+		Lib.current.stage.addEventListener(Event.RESIZE, onResize);
+	}
+	
+	private function onResize(?E:Event):Void
+	{
+		bitmap.width = Lib.current.stage.stageWidth;
+		bitmap.height = Lib.current.stage.stageHeight;
+		
+		trace(game.x + ", " + game.y + "[w: " + game.width + ", h: " + game.height + "]");
 	}
 	
 	private function init(?E:Event):Void 
@@ -62,7 +85,9 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
+		
+		trace(gameWidth + "x" + gameHeight);
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 	}
 }
